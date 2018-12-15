de8cb053f25667a83019ab3def1342138825cc54
unknown <29978690+tstg@users.noreply.github.com>
2018-12-16 01:43:39 +0800

    first commit

---
+++ blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views import generic
+
+from .models import Blog
+
+
+# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'blog/blog_list.html'
+    context_object_name = 'blogs'
+
+    def get_queryset(self):
+        return Blog.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = Blog
+    template_name = 'blog/editor.html'
